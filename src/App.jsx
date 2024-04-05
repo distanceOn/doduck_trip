12e1ca5387f2818b88b21bf316f70214c326c2ca
Roman Kondrashev <distanceOn@yandex.ru>
2024-04-05 23:58:58 +0300

    fix: delete some files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import React from "react";
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HomePage, MapPage, ProfilePage } from "./page/index";
-import { Header } from "./components";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route path="/map" element={<MapPage />} />
