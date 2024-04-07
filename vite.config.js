237329d4d787b6e2ad6bfd4d66af6189953eb44a
Roman Kondrashev <distanceOn@yandex.ru>
2024-04-07 13:07:53 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+import postcss from "postcss";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    postcss({
+      plugins: [tailwindcss, autoprefixer],
+    }),
+  ],
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
+  build: {
+    target: "es2015",
+  },
+});
